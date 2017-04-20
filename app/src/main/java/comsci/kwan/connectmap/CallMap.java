90d720592532ca17548572e32c46a349f118f865
kwanpk <babypooh007@hotmail.com>
2017-04-20 12:11:43 +0700

    Connect Map Complete

--- app/src/main/java/comsci/kwan/connectmap/CallMap.java
+++ app/src/main/java/comsci/kwan/connectmap/CallMap.java
@@ -26,7 +26,7 @@ public class CallMap extends AppCompatActivity {
         String lng = longtitude.getText().toString().trim();
         String mlabel = label.getText().toString().trim();
 
-        Uri location = Uri.parse("http://maps.google.com/maps?z=10&q=loc:13.7589193,100.5384698 ศูนย์รางน้ำ");
+        Uri location = Uri.parse("http://maps.google.com/maps?z=10&q=loc:"+ lat + "," + lng + "( "+mlabel+")");
 
         Intent mapIntent = new Intent(Intent.ACTION_VIEW,location);
         // mapIntent.setPackage("com.google.android.apps.maps"); ใช้ได้กับแอนดรอย์เที่มี googlemap , ใช้ไม่ได้กับ genymotion
