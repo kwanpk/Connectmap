a2af527ec12daa35fb4f02cd2978db5422676cb3
kwanpk <babypooh007@hotmail.com>
2017-04-20 11:49:34 +0700

    Test Map

--- app/src/main/java/comsci/kwan/connectmap/CallMap.java
+++ app/src/main/java/comsci/kwan/connectmap/CallMap.java
@@ -1,7 +1,11 @@
 package comsci.kwan.connectmap;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class CallMap extends AppCompatActivity {
 
@@ -10,6 +14,27 @@ public class CallMap extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call_map);
     } //onCreate method
+
+    public void runCallMap(View view){ //รับค่าจาก view
+
+        //intitial view
+        EditText latitude = (EditText) findViewById(R.id.mapLat);
+        EditText longtitude = (EditText) findViewById(R.id.mapLong);
+        EditText label = (EditText) findViewById(R.id.mapLabel);
+
+        String lat = latitude.getText().toString().trim();
+        String lng = longtitude.getText().toString().trim();
+        String mlabel = label.getText().toString().trim();
+
+        Uri location = Uri.parse("http://maps.google.com/maps?z=10&q=loc:13.7589193,100.5384698 ศูนย์รางน้ำ");
+
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW,location);
+        // mapIntent.setPackage("com.google.android.apps.maps"); ใช้ได้กับแอนดรอย์เที่มี googlemap , ใช้ไม่ได้กับ genymotion
+        startActivity(mapIntent);
+
+
+
+    } //runcallmap
 
 
 } //main class
